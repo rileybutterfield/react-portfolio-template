200aed3def19ba400414b3e6977f6dfd6ae7bdcd
Riley Butterfield <rileybutterfield1@gmail.com>
2021-04-05 17:47:37 -0400

    small updates

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -6,14 +6,6 @@ class ContactForm extends React.Component {
     }
     const buttonsStyle = {
       textAlign: "center",
-      // margin: '50px',
-      // padding: '10px',
-      // backgroundColor: 'black',
-      // color: 'white',
-      // border: '0px',
-      // transition: 'all 0.2s ease-in-out',
-      // textDecoration: 'none',
-      // cursor: 'pointer',
     }
     return (
       <form style={formStyle} className="form" action={this.props.action} method="post">
@@ -30,9 +22,7 @@ class ContactForm extends React.Component {
           <textarea name="message" rows="5"></textarea>
         </div>
         <div className="actions" style={buttonsStyle}>
-
               <button type="submit"  className="primary-btn">Send</button>
-
         </div>
       </form>
     );
